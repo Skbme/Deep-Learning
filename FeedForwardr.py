0baa50037be459c9f48d3871e125b1f98ffed30a
Skbme <sk.kbme@gmail.com>
2017-06-29 23:55:26 +0900

    test

--- FeedForwardr.py
+++ FeedForwardr.py
@@ -10,10 +10,30 @@ class neuron:
     #output y = f(wx+b)
 
         lamda = self.w*input +self.b
-        return self.getAct(lamda)
+        output = self.getAct(lamda)
+        self.input  = input
+        self.output = output
+
+        return output
+
     def getAct(self,x):
         return max(0.0,x)
 
+    def getActgrad(self,x):
+        if x>0.0:
+            return x
+        else:    
+            return 0.0
+
+    def propbackword(self,target):
+        a=0.1
+        self.w = self.w - a*(self.output-target)*self.getActgrad(self.output)*self.input
+        self.b = self.b - a*(self.output-target)*self.getActgrad(self.output)
+
 neuron = neuron(2.0,1.0)
-print ('input 1.0 -> output {}'.format(neuron.feedforward(1.0)))
-print ('input 2.0 -> output {}'.format(neuron.feedforward(2.0)))
+
+# print ('input 1.0 -> output {}'.format(neuron.feedforward(1.0)))
+# print ('input 2.0 -> output {}'.format(neuron.feedforward(2.0)))
+for i in range(1,100):
+    print ('input 1.0 -> output{}'.format(neuron.feedforward(1.0)))
+    neuron.propbackword(4.0)
